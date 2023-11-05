e75f602ea664c5ea657b07dab2bdc3b306f4aa1c
JerrickBan <96323262+JerrickBan@users.noreply.github.com>
2023-11-05 15:54:44 -0500

    First push

---
+++ classes.py
@@ -0,0 +1,254 @@
+import random
+import collections
+from enum import Enum
+import os
+import random
+import csv
+
+class Phase(Enum):
+    DRAW = 1
+    STANBY = 2
+    MAIN = 3
+    BATTLE = 4
+    END = 5
+
+class Face(Enum):
+    Up = 1
+    Down = 0
+
+class Turn(Enum):
+    YOUR = 1
+    OPPONENTS = 0
+
+
+class MonsterCard:
+    def __init__(self, name, level, type, attribute, atk, defense):
+        self.name = name
+        self.level = level
+        self.type = type
+        self.attribute = attribute
+        self.atk = atk
+        self.defense = defense
+        self.face = Face.Down
+    def display(self):
+        name_level = f'{self.name:7} (lv {self.level})'
+        str_atk = f'ATK:{self.atk}'
+        str_def = f'DEF:{self.defense}'
+        maxlen = max(len(name_level), len(str_atk), len(str_def))
+        border = '*' + '-' * maxlen + '*' 
+        print(border)
+        print(f'|{name_level}{" " * (maxlen - len(name_level))}|')
+        print(f'|{str_atk}{" " * (maxlen - len(str_atk))}|')
+        print(f'|{str_def}{" " * (maxlen - len(str_def))}|')
+        print(border)
+
+
+class Deck:
+    def __init__(self): # arr: list of card names, info: dictionary for other data about card
+        self.cards = collections.deque([]) # stack where last card is top card
+        self.card_count = collections.defaultdict(int)
+
+    def init_deck(self, cards):
+        for c in cards:
+            self.cards.append(c)
+                
+    def valid_deck_len(self):
+        return len(self.cards) >= 20 and len(self.cards) <= 30
+
+    def add_card(self, card):
+        if self.card_count[card.name] < 3:
+            self.cards.append(card)
+            self.card_count[card.name] += 1
+        else:
+            print("Only 3 copies per card allowed!")
+    
+    def remove_card(self, card):
+        if self.card_count[card.name] > 0:
+            self.cards.remove(card)
+            self.card_count[card.name] -= 1
+            if self.card_count[card.name] == 0:
+                del self.card_count[card.name]
+        else:
+            print(f"{card.name} not in deck!")
+    
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def draw(self):
+        return self.cards.pop()
+
+    def peek(self, num): # number of cards to look at from the top of the deck
+        if num > len(self.cards): return []
+        return [self.cards[i] for i in range(len(self.cards)-1,len(self.cards)-1-num, -1)]
+
+    def put_on_top(self, card: MonsterCard):
+        self.arr.append(card)
+    
+    def put_into_deck(self, card: MonsterCard):
+        self.arr.append(card) 
+        self.shuffle(self.arr)   
+
+    def put_on_bottom(self, card: MonsterCard):
+        self.arr.appendleft(card)
+
+    def search(self, name):
+        for i, card in enumerate(self.cards):
+            if card.name == name:
+                return self.cards.pop(i)
+            
+        return None
+    
+    def print_deck(self):
+        print(f'{len(self.cards)} Cards')
+        for name, cnt in self.card_count.items():
+            print(f'\t{name}  x{cnt}')
+
+class Actions:
+    def __init__(self):
+        self.num_norm_sum = 0
+        self.norm_sum_limit = 1
+
+    def normal_summon():
+        pass
+
+    def normal_set():
+        pass
+
+    def tribute_summon():
+        pass
+
+    def tribute_set():
+        pass
+
+    def activate_spell():
+        pass
+
+    def set_spell_trap():
+        pass
+
+    def check_player_grave():
+        pass
+
+    def check_opponent_grave():
+        pass
+
+
+class BoardState:
+    def __init__(self):
+        self.om = [None,None,None,None,None] # monster zones
+        self.ost = [None,None,None,None,None] # spell/trap zones
+        self.oh = set() # Opponent hand
+        self.og = set() # graveyard
+        self.ob = set() # banished
+        self.of = None # field spell
+        self.pm = [None,None,None,None,None]
+        self.pst = [None,None,None,None,None]
+        self.ph = set()
+        self.pg = set()
+        self.pb = set()
+        self.pf = None
+
+    def print_board(self, phase, turn, plp, olp, player):
+        os.system('clear')
+        print(f"\n{' ':25}{len(self.oh)} cards in OPPONENT's hand\n")
+        divider = '|----------|----------|----------|----------|----------|----------|----------|\n'
+        empty = "|          "
+        grave = '|   grave  '
+        deck = '|   deck   '
+        field = '|   field  '
+        banish = '|  banish  '
+
+        ## OPPONENT SIDE
+        print(divider)
+        print(deck, end='')
+        for card in self.ost:
+            if card:
+                print(f"|{card.name:10}", end="")
+            else:
+                print(empty, end="")
+        print(banish,end="")
+        print('|\n')
+        print(divider)
+        print(grave, end='')
+        for card in self.om:
+            if card:
+                print(f"|{card:5}", end="")
+            else:
+                print(empty, end="")
+        print(field, end="")
+        print('|\n')
+        print(divider)
+
+        print(f"\n{' ':50}OPPONENT'S Life Points: {olp}\n")
+        print(f'\n{" ":20}{player.name} {phase.name} PHASE {" ":13} Turn: {turn}{" ":13}\n')
+
+        print(f"\nYOUR Life Points: {plp}\n")
+        ## PLAYER SIDE
+        print(divider)
+        print(field, end="")
+        for card in self.pst:
+            if card:
+                print(f"|{card:5}", end="")
+            else:
+                print(empty, end="")
+        print(grave, end='')
+        print('|\n')
+        print(divider)
+        print(banish, end="")
+        for card in self.pm:
+            if card:
+                print(f"|{card:5}", end="")
+            else:
+                print(empty, end="")
+        print(deck, end='')
+        print('|\n')
+        print(divider)
+
+        print(f'\n{" ":10}YOUR hand: {",".join(self.ph)}\n')
+        
+
+
+class PlayGame:
+    def __init__(self, player_deck: Deck, opponent_deck: Deck):
+        self.pdeck = player_deck
+        self.odeck = opponent_deck
+        self.lp = 4000 #life points
+        self.olp = 4000
+        self.turn = 1
+        self.active = Turn.YOUR
+        self.phase = Phase.DRAW
+        self.b = BoardState()
+
+        def start_game(self):
+            self.active = Turn(random.choice([0,1])) # select starting player
+
+
+
+class CardData:
+    def __init__(self):
+        self.lines = []
+        self.name_map = {}
+
+    def get_data(self):
+        with open("card_data.csv") as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=',')
+            for row in spamreader:
+                self.lines.append(row)
+
+        for n, t, l, r, a, atk, d in self.lines:
+            self.name_map[n] = {"Type": t, "Level": l, "Race":r, "Attribute":a, "ATK": atk, "DEF":d}
+
+    
+    def filter(self, map, attr, value):
+        return {name:val for name, val in map.items() if val[attr] == value}
+    
+    def dict_to_cards(self, card_dict):
+        card_list = [n for n , _ in card_dict.items()]
+        return [MonsterCard(card,  card_dict[card]['Level'],card_dict[card]['Race'], card_dict[card]['Attribute'], card_dict[card]['ATK'], card_dict[card]['DEF']) for card in card_list]
+
+    def get_cards_of_attribute(self, info, attr):
+        card_pool = self.filter(info, "Race", attr)
+        return self.dict_to_cards(card_pool) * 3
+
+
+    
---
+++ yugioh.py
@@ -0,0 +1,189 @@
+#!/usr/bin/python3
+import csv
+import random
+import os
+from classes import BoardState, Phase, MonsterCard, Deck, Actions, Turn, CardData, PlayGame
+
+
+################################################################################
+#################### GLOBAL VARIABLES ##########################################
+################################################################################
+
+types = {
+    "Aqua",
+    "Beast",
+    "Beast-Warrior",
+    "Cyberse",
+    "Dinosaur",
+    "Divine-Beast",
+    "Dragon",
+    "Fairy",
+    "Fiend",
+    "Fish",
+    "Insect",
+    "Machine",
+    "Plant",
+    "Psychic",
+    "Pyro",
+    "Reptile",
+    "Rock",
+    "Sea Serpent",
+    "Spellcaster",
+    "Thunder",
+    "Warrior",
+    "Winged Beast",
+    "Wyrm",
+    "Zombie"
+}
+type_str = "\n".join(sorted(list(types)))
+
+
+
+################################################################################
+#################### LOAD CSV DATASET ##########################################
+################################################################################
+c = CardData()
+c.get_data()
+info = c.filter(c.name_map,"Type", "Normal Monster" )
+
+os.system("clear")
+print("Welcome to the YU-GI-OH! terminal trading card game!")
+s = input("Type 's' to start: ")
+while s != 's':
+    s = input("Type 's' to start: ")
+
+
+################################################################################
+#################### INSTRUCTIONS ##############################################
+################################################################################
+os.system("clear")
+print("Rules:")
+print("  1. Players start with 5 cards in their hand\n")
+print("  2. There are 5 Phases of play: Draw, Standby, Main, Battle, and End")
+print("\tPlayers Draw 1 card during the Draw Phase except the first player on the first turn")
+print("\tStanby Phase is for effects that specify this phase, but in this game it won't be used for simplicity")
+print("\tThe Main Phase is where you do all your actions including summoning, activiating, setting cards")
+print("\tThe Battle Phase is where you attack your opponents monsters with your own")
+print("\tThe End Phase signals the end of your turn and effects that specific this phase will activate; again not used\n")
+print("  3. Each player's board has 3 monster zones, 3 spell and trap zones, a deck zone, a field spell zone, and graveyard, and a banished zone\n")
+print("  4. The deck goes in the deck zone, and cards that are destroyed are sent to the graveyard. Banished cards are cards that are removed from play\n")
+print("  5. Each player starts with 4000 life points\n")
+print("  6. To win the game, your opponent's life points must reach 0 must or they run out of cards in their deck\n")
+
+
+s = input("Type 'n' to continue to Phase Rules: ")
+while s != 's':
+    s = input("Type 'n' to continue to Main and Battle Phase Rules: ")
+
+os.system("clear")
+print("Main Phase Rules:")
+print("  1. You can only normal summon a monster 1 time per turn\n")
+print("  2. To normal summon level 5-6 monsters you must sacrifice 1 monster, and for 7+ you must sacrifice 2\n")
+print("  3. Monsters can be in face-up attack position (horizontal) or face-up or face-down defense position (horizontal)\n")
+print("  4. You can also choose to normal set a monster in face down defense defense position\n")
+print("  5. You can change the battle position of each monster max 1 time per turn. Defense positions must go to attack and attack must go to face-up defense\n")
+
+
+print(" ")
+
+
+s = input("Type 'n' to continue: ")
+while s != 's':
+    s = input("Type 'n' to continue: ")
+
+
+################################################################################
+#################### SELECT DECK ###############################################
+################################################################################
+
+# Selecting the AI Deck
+ai_attribute = random.choice(list(types))
+ai_cards = c.get_cards_of_attribute(info, ai_attribute)
+while(len(ai_cards) < 20):
+    ai_attribute = random.choice(list(types))
+    ai_cards = c.get_cards_of_attribute(info, ai_attribute)
+
+ai_deck = Deck()
+ai_deck.init_deck(random.sample(ai_cards * 3, 20))
+ai_deck.cards[0].display()
+
+os.system("clear")
+print("You will now enter Deck Building.\nDecks must be 20-30 cards\nMax 3 duplicates per card")
+n = input("Type 'n' to continue: ")
+while n != 'n':
+    n = input("Type 'n' to continue: ")
+
+
+# Selecting the User Deck type
+os.system("clear")
+print('Select a Monster Attribute to build your deck around.')
+print(f'All Monster Attributes:\n{type_str}')
+t = input(f'Type for your deck: ')
+while (t not in types or len(c.get_cards_of_attribute(info, t)) < 20):
+    t = input(f'Type for your deck: ')
+    if len(c.get_cards_of_attribute(info, t)) < 20:
+        print(f"\tNot enough cards of type {t} to make a deck!")
+        continue
+
+card_pool = c.filter(info, "Race", t)
+player_cards = c.dict_to_cards(card_pool)
+
+# Card Selection Menu
+index = 0
+deck = Deck()
+os.system('clear')
+print("Actions:\nn: next card\np: previous card\na: add card\nr: remove current card\nc: clear history\nf: finished\nd: display deck")
+while(True):
+    player_cards[index].display()
+
+    #print deck
+    option = input("Select Option: ")
+
+    if option == 'f':
+        if (deck.valid_deck_len()):
+            break
+        else:
+            print("Deck must be 20-30 cards!")
+    if option == 'q':
+        break
+    elif option == 'a':
+        deck.add_card(player_cards[index])
+    elif option == 'r':
+        deck.remove_card(player_cards[index])
+    elif option == 'n':
+        if index < len(player_cards) - 1:
+            index+=1
+        else: print("At end of cards")
+    elif option == 'p':
+        if index > 0:
+            index-=1
+        else: print("At beginning of cards")
+    elif option == 'c':
+        os.system('clear')
+        print("Actions:\nn: next card\np: previous card\na: add card\nr name: remove card name\nc: clear history")
+    elif option == 'd':
+        deck.print_deck()
+        
+    
+
+
+
+
+
+
+# index = 0
+# os.system("clear")
+# print(f"Cards with Attribute {t}\nn: next\np: previous\na: add to deck\nr [name] to remove from deck\nr: ready")
+# card_pool[card_names[index]].display()
+# action = input("Action: ")
+# while action != 'q':
+#     card_pool[card_names[index]].display()
+
+# print(f'Cards with Attribute {t}:')
+# for i, name in enumerate(card_names):
+#     print(f'{i}: {name}')
+
+
+
+# b = BoardState()
+# b.print_board(Phase.DRAW, 1, 4000,4000, Turn(0))
